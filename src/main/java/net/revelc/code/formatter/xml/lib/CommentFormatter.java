ac6ec9c798ef954b4ca7e9f0e75af6d7a122fb17
Jeremy Landis <jeremylandis@hotmail.com>
2024-04-05 22:49:19 -0400

    [enhance] Rewrite handler for comment formatter
    - inline license header is well known to be extremely buggy.  Exclude the output files as it will end up adding multiple headers.
    - All items in the expected items are tab based, the tests are tab based, make sure only tabs are being output there including the license header.
    - Pass preferences to the comment handler as is done with other handler
    - Completely rewrite comment handler.  It does not need a pattern.
    
    For the rewrite, here is how it handles
    - For single line items, it will just run single indent as was original logic
    - For <!-- only, --> only, or starts with <!--, it will strip leading spacing and add original indent as was original case.
    - If indent comes in with no indention and line is physically not empty, then use the canonical indent ( these are headers at least here and need tabbed in with actual spacing not empty )
    - For all others do a double indent.
    
    For all here this works well and file output looks coherent in that it uses all same styling throughout.  The license plugin did trip this up, so that was fixed by ignoring them as it will conflict with all other files and double headers result.  So avoiding that outright.

--- src/main/java/net/revelc/code/formatter/xml/lib/CommentFormatter.java
+++ src/main/java/net/revelc/code/formatter/xml/lib/CommentFormatter.java
@@ -8,50 +8,45 @@
 package net.revelc.code.formatter.xml.lib;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CommentFormatter {
 
-    private static final Pattern ORIGINAL_INDENT_PATTERN = Pattern.compile("^(?<indent>\\s*)-->");
+    private static final Logger logger = LoggerFactory.getLogger(CommentFormatter.class);
 
-    public String format(String tagText, String indent, String lineDelimiter) {
+    public String format(String tagText, String indent, String lineDelimiter, FormattingPreferences prefs) {
         String[] lines = tagText.split(lineDelimiter, -1);
-        String originalIndent = resolveOriginalIndent(lines);
 
-        List<String> newLines = new ArrayList<>();
-        for (String line : lines) {
-            newLines.add(indent + removeOriginalIndent(line, originalIndent));
+        if (logger.isDebugEnabled()) {
+            logger.debug("input: {}\n", Arrays.toString(lines));
         }
 
+        // Caller sets initial indents to method (no indent before <!-- and indent before -->)
+        List<String> newLines = new ArrayList<>();
+        for (String line : lines) {
+            // do not trim leading space on multi-line comments (just add one indent)
+            if (lines.length < 2) {
+                newLines.add(indent + line);
+            } else if (line.trim().equals("<!--") || line.trim().equals("-->") || line.contains("<!--")) {
+                // add one indent for begin comment / end comment / line starting comment with text
+                // allowing last line with comment to be indented twice on else
+                newLines.add(indent + line.stripLeading());
+            } else if (indent.equals("") && !line.stripLeading().equals("")) {
+                // If indent is empty and line is not empty, use canonicalIndent
+                newLines.add(prefs.getCanonicalIndent() + line.stripLeading());
+            } else {
+                // Add two intent for others
+                newLines.add(indent + indent + line.stripLeading());
+            }
+        }
+
+        logger.debug("output: {}\n", newLines);
+
+        // Returned data to caller will be properly positioned by caller
         return String.join(lineDelimiter, newLines);
     }
 
-    private String resolveOriginalIndent(String[] lines) {
-        // only multi-line comments need replace original indentation.
-        if (lines.length < 2) {
-            return null;
-        }
-
-        for (int i = lines.length - 1; i >= 0; i--) {
-            String line = lines[i];
-
-            if (line.trim().endsWith("-->")) {
-                Matcher m = ORIGINAL_INDENT_PATTERN.matcher(line);
-                if (m.matches()) {
-                    return m.group("indent");
-                }
-                return null;
-            }
-        }
-        return null;
-    }
-
-    private static String removeOriginalIndent(String line, String indent) {
-        if (indent != null && line.startsWith(indent)) {
-            return line.substring(indent.length());
-        }
-        return line;
-    }
 }
--- src/main/java/net/revelc/code/formatter/xml/lib/XmlDocumentFormatter.java
+++ src/main/java/net/revelc/code/formatter/xml/lib/XmlDocumentFormatter.java
@@ -78,8 +78,8 @@ public class XmlDocumentFormatter {
         } else if (tag instanceof CommentReader) {
             StringBuilder indentBuilder = new StringBuilder(30);
             indent(state.depth, indentBuilder);
-            state.out.append(
-                    new CommentFormatter().format(tag.getTagText(), indentBuilder.toString(), fDefaultLineDelimiter));
+            state.out.append(new CommentFormatter().format(tag.getTagText(), indentBuilder.toString(),
+                    fDefaultLineDelimiter, prefs));
         } else {
             String tagText = tag.getTagText();
             if (!prefs.getDeleteBlankLines()
